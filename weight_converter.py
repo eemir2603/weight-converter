1ce59e5174f141c12bbea897fb9ca7cfa2608d39
El'Castillo🐧 <eemirutku59@gmail.com>
2026-08-07 02:30:22 +0300

    Add print statements for weight conversion results
    Added print statements to display converted weight.

--- weight_converter.py
+++ weight_converter.py
@@ -6,11 +6,14 @@ unit = input("Is this weight in (K)g or (L)bs? ").upper()
 if unit == "K":
     weight = weight * 2.205
     unit = "Lbs."
+    print (f"Your weight is: {weight:.2f} {unit} ")
+
 elif unit == "L":
     weight = weight / 2.20590
     unit = "Kg"
+    print (f"Your weight is: {weight:.2f} {unit} ")
+
 else:
     print (f"{unit} is not valid ") 
 
-print (f"Your weight is: {weight:.2f} {unit} ")
 exit()
